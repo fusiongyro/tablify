f9e687569689f41599ec5acb2b2210213eb5d99a
Daniel Lyons <fusion@storytotell.org>
2010-02-01 13:01:53 -0800

    Switched to XHtml combinator library for HTML generation. Should be safer.

--- src/HTML.hs
+++ src/HTML.hs
@@ -1,61 +1,17 @@
 module HTML (converter) where
 
+import Text.XHtml.Strict
 import Text.Regex
 import Utilities
 import Converter
 
-data Tag = Tag { tagName :: String, tagBody :: Tag } 
-         | TList [Tag]
-         | Body String
-        deriving Show
-
-indent :: Integer -> String
-indent n = genericReplicate (n*2) ' '
-
-start :: String -> String
-start s = "<" ++ s ++ ">"
-
-end :: String -> String
-end s = "</" ++ s ++ ">"
-
--- escapes the bare minimum of characters for safe embedding in HTML
-htmlEscape :: String -> String
-htmlEscape html = foldl' process html mappings
-    where
-        mappings = [("&", "&amp;"), ("<", "&lt;"), (">", "&gt;"), ("\n", "<br />")]
-        process text (find, replace) = subRegex (mkRegex find) text replace
-
-renderTag :: Integer -> Tag -> String
-renderTag l (Tag name (Body bod)) =
-    indent l ++ start name ++ htmlEscape bod ++ end name ++ "\n"
-renderTag l (Tag name (TList tags)) =
-       indent l ++ start name ++ "\n" 
-    ++ (concatMap (renderTag (succ l)) tags) 
-    ++ indent l ++ end name ++ "\n"
-renderTag l (Tag name bod) =
-       indent l 
-    ++ start name ++ "\n" 
-    ++ renderTag (succ l) bod 
-    ++ indent l ++ end name ++ "\n"
-renderTag level (Body bod) = indent level ++ htmlEscape bod
-
-render :: Tag -> String
-render = renderTag 0
-
 htmlify :: Table -> String
-htmlify table = render tableToTags
-    where
-        tableToTags = Tag "table" (TList [header, body])
-                
-        header, body :: Tag
-        header = Tag "thead" $ rowToTags "th" (head table)
-        body   = Tag "tbody" $ TList $ 
-                    map (rowToTags "td") (tail table)
-        
-        rowToTags :: String -> [String] -> Tag
-        rowToTags cellT row = Tag "tr" (TList $ map (cellToTag cellT) row)
-        
-        cellToTag :: String -> String -> Tag
-        cellToTag cellT cell = Tag cellT (Body cell)
+htmlify tbl = prettyHtml $ table << (concatHtml [thead << header, tbody << body])
+  where
+    header = tr << (rowToTr th $ head tbl)
+    row r  = tr << (rowToTr td r)
+    body   = map row $ tail tbl
+
+    rowToTr cellType = map (cellType <<)
 
 converter = Converter "HTML" htmlify "H" "html"
