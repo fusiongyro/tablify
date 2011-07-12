b711e04796f840dc1c9a69d69c40e351c2e65550
Daniel Lyons <fusion@storytotell.org>
2011-07-11 19:14:35 -0600

    bumping version number, moving some utilities into Utilities

--- src/FixedWidth.hs
+++ src/FixedWidth.hs
@@ -4,13 +4,13 @@ module FixedWidth
     (Joints, fixedWidthTable) where
 
 import Prelude hiding (length, replicate, (++))
-import Data.List hiding (length, replicate, (++))
+import Data.List hiding (length, replicate, intercalate, (++))
 import qualified Data.ByteString.Char8 as B
 
 import Utilities
 
 pad :: Int -> ByteString -> ByteString
-pad n str = str ++ B.replicate (n - B.length str) ' '
+pad n str = str ++ replicate (n - B.length str) ' '
 
 -- given a table, return a list of the maximum widths of each column
 columnWidths :: Table -> [Int]
@@ -19,7 +19,7 @@ columnWidths table = map (maximum . map B.length) $ transpose table
 boxRow :: [Int] -> Char -> ByteString -> ByteString -> ByteString -> ByteString
 boxRow widths sp left mid right = 
        left 
-    ++ B.intercalate mid (map (\x -> B.replicate (x+2) sp) widths)
+    ++ intercalate mid (map (\x -> replicate (x+2) sp) widths)
     ++ right
 
 type Joints = (ByteString,ByteString,ByteString)
@@ -30,7 +30,7 @@ surround c s = (c `B.cons` s) `B.snoc` c
 -- We used to call this ASCII art, but these days it's Unicode.
 fixedWidthTable :: Joints -> Joints -> Joints -> Char -> Char -> Table -> ByteString
 fixedWidthTable (tl,tm,tr) (ml, mm, mr) (bl, bm, br) h v table = 
-        B.intercalate "\n" [btop, bhead, bmid, body, bbot]
+        intercalate "\n" [btop, bhead, bmid, body, bbot]
     where
         -- I'm pattern matching the (0,0) as a way of 
         -- checking that the table is valid
@@ -42,9 +42,9 @@ fixedWidthTable (tl,tm,tr) (ml, mm, mr) (bl, bm, br) h v table =
         
         bhead, body :: ByteString
         bhead = formatRow (head table)
-        body  = B.intercalate "\n" $ map formatRow (tail table)
+        body  = intercalate "\n" $ map formatRow (tail table)
 
-        formatRow row = surround v $ B.intercalate (B.singleton v) $ 
+        formatRow row = surround v $ intercalate (B.singleton v) $ 
                         zipWith formatCell row widths
         formatCell :: ByteString -> Int -> ByteString
         formatCell val width = surround ' ' $ pad width val
--- src/LaTeX.hs
+++ src/LaTeX.hs
@@ -11,13 +11,13 @@ import Converter
 
 -- Some TeX.
 texify :: Table -> ByteString
-texify table = B.intercalate "\n" [tblstart, body, tblend]
+texify table = intercalate "\n" [tblstart, body, tblend]
 	where
 		columns = length $ head table
 		tblstart = "\\begin{tabular}" ++ "{" ++ heading 'l' ++ "}"
 		tblend = "\\end{tabular}"
-		heading c = B.intersperse ' ' $ B.replicate columns c
-		body = B.intercalate "\\\\\n" $ map (B.intercalate " & ") table
+		heading c = intersperse ' ' $ replicate columns c
+		body = intercalate "\\\\\n" $ map (intercalate " & ") table
 
 converter :: Converter
 converter = Converter "LaTeX" texify "L" "latex"
